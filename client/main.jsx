aa426541ef654b2bc69d298f608d829f97bd51a7
Maksym <maksymgithub@gmail.com>
2016-04-25 20:06:01 -0700

    broken code. Routing does not work

--- client/main.jsx
+++ client/main.jsx
@@ -1,6 +1,9 @@
 // ES6 SYNTAX FOR IMPORTING MODULES
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Router, hashHistory } from 'react-router';
+import About from './about';
+import Repos from './repo'
 
 // ES6 SYNTAX FOR IMPORTING CUSTOM EXPORTS FROM A MODULE
 // import { SOUND_CLOUD_KEY } from './config';
@@ -16,6 +19,10 @@ class App extends React.Component {
     return (
       <div>
         <Inner />
+        <Router history={hashHistory}>
+          <Router path="/" component={App} />
+        </Router>
+    
       </div>
     );
   }
